390cb7c685ac9c0fc0012eea9875ebc258f2236a
chamoe2005 <124413962+chamoe2005@users.noreply.github.com>
2023-02-03 21:32:06 -0500

    Add files via upload

---
+++ bgc_script_v1.lua
@@ -0,0 +1,38 @@
+
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+
+
+local wally = loadstring(game:HttpGet(("https://raw.githubusercontent.com/chamoe2005/script/main/wally"),true))()
+local farm = wally:CreateWindow('Auto Farm')
+
+	farm:Section("Farm")
+    farm:Toggle('Auto-Bubble Blow', {flag = 'Bubble'})
+    
+local library = require(game.ReplicatedStorage:WaitForChild("Nevermore"):WaitForChild("Library"))
+	
+spawn(function()
+
+	while(wait()) do
+		local ohTable1 = 
+		{
+			[1] = {
+				[1] = false
+			},
+			[2] = {
+				[1] = 2
+			}
+		}
+
+		if farm.flags.Bubble then
+			if library.Variables.AutoClicker == false then
+				--game:GetService("ReplicatedStorage").Remotes["blow bubble"]:FireServer(ohTable1)
+				library.Variables.AutoClicker = true
+			end
+		end
+	end
+end)
