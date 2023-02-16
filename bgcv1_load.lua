f9a342b5424aaf7b73130f3b1e9fcd6a3dcc7efb
chamoe2005 <124413962+chamoe2005@users.noreply.github.com>
2023-02-15 21:01:15 -0500

    Add files via upload

--- bgcv1_load.lua
+++ bgcv1_load.lua
@@ -1,1 +1,10 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/chamoe2005/script/main/bgc_script_v1.lua'))()
+_G.autoloadsettings = true
+
+print("BGS Script Loader Started")
+
+for i = 1, 15 do
+	wait(1)
+	print(15 - i)
+end
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/chamoe2005/script/main/bgc_script_v1.lua"),true))()
