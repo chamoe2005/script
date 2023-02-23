ffd294d323e2528c28e731d1cd83ce04e0788e69
chamoe2005 <124413962+chamoe2005@users.noreply.github.com>
2023-02-23 09:39:18 -0500

    Add files via upload

--- bgc_script_v1.lua
+++ bgc_script_v1.lua
@@ -1,4 +1,4 @@
-print("Version 2.1.1.2")
+print("Version 2.1.2.2")
 
 _G.settingsloaded = false
 _G.DisabledEggs = {"Valentine's 2023 Egg"}
@@ -1391,7 +1391,8 @@ spawn(function()
 						break
 					end
 				end
-				
+			elseif _G.BuyEggMode == "None"
+				library.Variables.AutoHatchEggId = nil
 			end
 			--wait(_G.TeleportDelay)
 		--end
