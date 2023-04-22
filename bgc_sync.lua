4e0ed87f148cf614f828ce311adf7b8036d8f10d
chamoe2005 <124413962+chamoe2005@users.noreply.github.com>
2023-04-22 05:02:31 -0400

    Add files via upload

---
+++ bgc_sync.lua
@@ -0,0 +1,39 @@
+function()
+
+	local sync = {["xyzzy1111111127"] = 
+					{"Auto-Bubble Blow V2" = true,
+					 "Sell Bubble Delay" = "600",
+					 "Sell Bubble Area" = "Sell 2",
+					 "Free Prize Wheel" = true,
+					 "Free Loot" = true,
+					 "Claim Bubble Pass" = true,
+					 "Fairy Exchange" = true,
+					 "Spawn World Egg Quests" = true,
+					 "Equip Best" = true,
+					 "Auto Claim" = true,
+					 "2x Coins " = true,
+					 "2x Diamonds " = true,
+					 "3x Shiny Luck " = true,
+					 "Fast Hatch " = true,
+					 "Mega Luck " = true,
+					 "Super Lucky " = true,
+					 "1 Pet Level Use" = true,
+					 "Power 1 Use" = true,
+					 "Power 2 Use" = true,
+					 "Hide Animation" = true,
+					 "Buy Mode" = "Best",
+					 "Common Egg" = true,
+					 "Season 3 Egg" = true,
+					 "Sky Island" = true,
+					 "Heaven" = true,
+					 "Space" = true,
+					 "The Void" = true,
+					 "Lucky Landing" = true,
+					 "Fairy World" = true,
+					 "VIP" = true}
+					}
+
+	return sync
+				
+
+end
