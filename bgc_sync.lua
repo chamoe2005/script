7ad034a4cecc1fe669be7dc4a96c29f17ea6e8ea
chamoe2005 <124413962+chamoe2005@users.noreply.github.com>
2023-05-01 15:43:02 -0400

    Update bgc_sync.lua

--- bgc_sync.lua
+++ bgc_sync.lua
@@ -1,6 +1,6 @@
 
 --function()
-	local version = 25
+	local version = 26
 
 	local sync = {["sync"] = 
 					{["Auto-Bubble Blow V2"] = true,
@@ -26,7 +26,7 @@
 					 ["Power 1 Brew"] = false,
 					 ["Power 2 Brew"] = false,
 					 ["Power 3 Brew"] = false,
-					 ["Bubbles Brew"] = true,
+					 ["Bubbles Brew"] = false,
 					 ["Shadow Brew"] = false,
 					 ["2x Coins "] = true,
 					 ["2x Diamonds "] = true,
